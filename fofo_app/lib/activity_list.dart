d4aec7e99ea1a119f9b84b582142badc8f711cf6
jhj04 <hj650272@gmail.com>
2024-05-01 17:28:20 +0900

    <feet> mainpage, activity_list appbar

--- fofo_app/lib/activity_list.dart
+++ fofo_app/lib/activity_list.dart
@@ -5,6 +5,42 @@ class Activity_List extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                // padding: EdgeInsets.all(15),
+                height: 100,
+                child: Center(
+                    child: Text(
+                        "Fofol",
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontFamily: 'YES24GothicR',
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          color: Color(0xFF647087),
+                        ),
+                      ),
+                  ),
+              ),
+              Container(
+                alignment: Alignment.topRight,
+                margin: EdgeInsets.all(20),
+                child: IconButton(
+                  icon: Icon(Icons.settings),
+                  iconSize: 50,
+                  onPressed: (){},
+                ),
+              ),
+            ],
+          ),],
+      ),
+    );
   }
 }
+
+
--- fofo_app/lib/main.dart
+++ fofo_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fofo_app/mainpage.dart';
 import 'signup.dart';
 
 void main(){
