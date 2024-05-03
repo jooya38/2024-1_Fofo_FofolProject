6e9bc53dea9a2160fddc76a3fb64b9da15908b3d
dasom040819 <da1ce10819@gmail.com>
2024-05-04 00:28:19 +0900

    add pubspec

--- fofo_app/lib/main.dart
+++ fofo_app/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:fofo_app/setting.dart';
 import 'signup.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'signin.dart';
 
 void main() {
-  initializeDateFormatting().then((_) => runApp(const MyApp()));
+  //initializeDateFormatting().then((_) => runApp(const MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Fofol',
       // home : Signup(),
-      home: Setting(),
+      home: LoginScreen(),
     );
   }
 }
