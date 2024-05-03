65b173e2cbe6309f4b9715dc8a1388b299433190
dasom040819 <da1ce10819@gmail.com>
2024-05-03 22:48:32 +0900

    1차

--- fofo_app/lib/main.dart
+++ fofo_app/lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:fofo_app/setting.dart';
 import 'signup.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-void main(){
-  runApp(const MyApp());
+void main() {
+  initializeDateFormatting().then((_) => runApp(const MyApp()));
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp ({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title : 'Fofol',
+      title: 'Fofol',
       // home : Signup(),
-      home:Setting(),
+      home: Setting(),
     );
   }
 }
