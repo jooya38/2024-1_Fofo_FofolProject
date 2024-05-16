6372ca2c8caeabc9086bd2ca5ddc50b9443d1b23
Jooya38 <hyj0102809@gmail.com>
2024-05-17 01:22:12 +0900

    Style: Fix main

--- FofoWeb/fofo/src/After_Login/te/Main1.js
+++ FofoWeb/fofo/src/After_Login/te/Main1.js
@@ -1,10 +1,9 @@
 //메인 페이지 화면
 
 import Main_body from "./Main_body";
-import Main_leftnav from "./Main_nav";
+import Main_leftnav from "./Main_leftnav";
 // import Main_rightnav from "./Main_rightnav";
 import './Main.css';
-import './Main_nav.css';
 
 const Main = () => {
     return <div className="main-container">
